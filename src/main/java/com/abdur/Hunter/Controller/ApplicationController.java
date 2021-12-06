955cc8cd0b5e90a957067eeb8ea7ec5dec4adafc
Abdur Rehman <wabdulrehman@yahoo.com>
2021-12-06 19:44:07 +0000

    Controller Layer for application

---
+++ src/main/java/com/abdur/Hunter/Controller/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.abdur.Hunter.Controller;
+
+import com.abdur.Hunter.Model.Application;
+import com.abdur.Hunter.Service.ApplicationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+    private ApplicationService applicationService;
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping("/newApplication")
+    public void createApplication(@RequestBody Application application){
+        applicationService.createApplication(application);
+    }
+
+
+}
